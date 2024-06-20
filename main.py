162197d17eb163e69e6c4224ef6ea1f11fc68fc4
choonsam22 <148427160+choonsam22@users.noreply.github.com>
2024-06-20 13:09:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ if st.button('질문하기'):
             Use the following pieces of context to answer the users question.
             If you don't know the answer, just say that "I don't know", don't try to make up an answer.
             ----------------
-            :"""
+            {summaries}
+            You MUST answer in Korean :"""
             messages = [
                 SystemMessagePromptTemplate.from_template(system_template),
                 HumanMessagePromptTemplate.from_template("{question}")
